3423f3b2975e81f94a5f8b0592e44dbfc6042a09
Trevor Vannoy <trevorvannoy@montana.edu>
2023-10-27 13:23:52 -0600

    rename files and setup Julia environment

---
+++ mpi/create-mpi-environment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+module load Julia
+
+# Setup julia environment so we can isolate our packages
+ENV_NAME="mpi"
+julia -e "using Pkg; Pkg.activate(\""${ENV_NAME}"\"); Pkg.add(\"MPI\")"
+
+# Set MPI.jl to use the cluster's OpenMPI module.
+# In our testing, 4.1.4-GCC-11.3.0 is the latest OpenMPI version that works
+# on the legacy nodes.
+module load OpenMPI/4.1.4-GCC-11.3.0
+
+julia --project="${ENV_NAME}" -e 'using Pkg; Pkg.add("MPIPreferences")'
+julia --project="${ENV_NAME}" -e 'using MPIPreferences; MPIPreferences.use_system_binary()'
+
+module purge
