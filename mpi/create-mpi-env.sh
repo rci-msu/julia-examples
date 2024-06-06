27d61dad8f80ee4072583c8e6fee825ec1174747
Trevor Vannoy <trevorvannoy@montana.edu>
2024-06-06 12:06:11 -0600

    use openmpi_jll

--- mpi/create-mpi-env.sh
+++ mpi/create-mpi-env.sh
@@ -5,13 +5,15 @@ module load Julia
 # Setup julia environment so we can isolate our packages
 ENV_NAME="mpi"
 julia -e "using Pkg; Pkg.activate(\""${ENV_NAME}"\"); Pkg.add(\"MPI\")"
+julia --project="${ENV_NAME}" -e "using MPI; MPI.install_mpiexecjl()'"
 
 # Set MPI.jl to use the cluster's OpenMPI module.
 # In our testing, 4.1.4-GCC-11.3.0 is the latest OpenMPI version that works
 # on the legacy nodes.
-module load OpenMPI/4.1.4-GCC-11.3.0
+#module load OpenMPI/4.1.4-GCC-11.3.0
 
 julia --project="${ENV_NAME}" -e 'using Pkg; Pkg.add("MPIPreferences")'
-julia --project="${ENV_NAME}" -e 'using MPIPreferences; MPIPreferences.use_system_binary()'
+#julia --project="${ENV_NAME}" -e 'using MPIPreferences; MPIPreferences.use_system_binary()'
+julia --project="${ENV_NAME}" -e 'using MPIPreferences; MPIPreferences.use_jll_binary("OpenMPI_jll")'
 
 module purge
